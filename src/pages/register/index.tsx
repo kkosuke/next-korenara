ee1f27a196308ae798e0b5a5ad4d594ea6fa8827
kkosuke <kkosuke007@gmail.com>
2023-06-13 13:28:48 +0900

    slect要素のテスト

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -128,6 +128,23 @@ const RegisterIndex = () => {
             />
           </div>
           <div className="mt-4">
+            <label
+              className="mb-1 block text-sm font-medium text-gray-700"
+              htmlFor="example2"
+            >
+              言語（開発中）
+            </label>
+            <select
+              id="example2"
+              name="userLang"
+              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50"
+            >
+              <option value="">-</option>
+              <option value="jp">日本語</option>
+              <option value="en">English</option>
+            </select>
+          </div>
+          <div className="mt-4">
             <button
               type="submit"
               className="rounded-lg border border-primary-500 bg-primary-500 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300"
